39006019f06cd3af1be93bf3bf1979afc5ce4bb7
IAmSilK <iamsilk3495@gmail.com>
2021-06-14 17:46:58 -0300

    Add AsyncHelper

---
+++ ShopsUtility/AsyncHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ShopsUtility
+{
+    public static class AsyncHelper
+    {
+        public static void Run(Func<Task> task)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    await task();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+            });
+        }
+    }
+}
