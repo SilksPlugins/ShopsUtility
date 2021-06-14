264690086d155ec48f65a4a6d4e212292f9eef4e
IAmSilK <iamsilk3495@gmail.com>
2021-06-14 18:30:02 -0300

    Added shop models for UI listings

---
+++ ShopsUtility/Shops/ItemShop.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopsUtility.Shops
+{
+    public class ItemShop
+    {
+        public ushort ItemId { get; set; }
+
+        public string ItemName { get; set; }
+
+        public decimal? BuyPrice { get; set; }
+
+        public decimal? SellPrice { get; set; }
+
+        public int Order { get; set; }
+    }
+}
---
+++ ShopsUtility/Shops/VehicleShop.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopsUtility.Shops
+{
+    public class VehicleShop
+    {
+        public ushort VehicleId { get; set; }
+
+        public string VehicleName { get; set; }
+
+        public decimal BuyPrice { get; set; }
+
+        public int Order { get; set; }
+    }
+}
