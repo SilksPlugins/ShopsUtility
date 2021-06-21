8bf5dfcfb5b30b06d288155fa35c4e233c21ea07
Stephen White <iamsilk3495@gmail.com>
2021-06-21 00:10:16 -0300

    Added GroupTabBase

---
+++ ShopsUtility/Tabs/GroupTabBase.cs
@@ -0,0 +1,18 @@
+﻿using ShopsUtility.Database;
+
+namespace ShopsUtility.Tabs
+{
+    public abstract class GroupTabBase
+    {
+        public MainWindow Window { get; }
+
+        public ShopsDbContext DbContext { get; }
+
+        protected GroupTabBase(MainWindow window, string connectionString)
+        {
+            Window = window;
+
+            DbContext = new ShopsDbContext(connectionString);
+        }
+    }
+}
