5d710aa89b712adcfc2bd56f6bfb29df20088914
Stephen White <iamsilk3495@gmail.com>
2021-06-21 00:10:49 -0300

    Added ItemGroupTab and VehicleGroupTab code bases

---
+++ ShopsUtility/Tabs/ItemGroupTab.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors.Core;
+
+namespace ShopsUtility.Tabs
+{
+    public class ItemGroupTab : GroupTabBase
+    {
+        public ICommand ItemGroupRemovedCommand { get; set; }
+
+        public ItemGroupTab(MainWindow window, string connectionString) : base(window, connectionString)
+        {
+            ItemGroupRemovedCommand = new ActionCommand(x => ItemGroupRemoved((string) x));
+        }
+
+        private void ItemGroupRemoved(string itemGroup)
+        {
+
+        }
+    }
+}
---
+++ ShopsUtility/Tabs/VehicleGroupTab.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopsUtility.Tabs
+{
+    public class VehicleGroupTab : GroupTabBase
+    {
+        public VehicleGroupTab(MainWindow window, string connectionString) : base(window, connectionString)
+        {
+        }
+    }
+}
